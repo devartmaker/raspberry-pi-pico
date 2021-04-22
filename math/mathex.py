f40999713b1260844a3c8d0010e6032742af6b17
Philip choi <devartmaker@gmail.com>
2021-04-22 22:50:57 +0900

    change function name

--- math/mathex.py
+++ math/mathex.py
@@ -1,4 +1,4 @@
-def map(x, in_min, in_max, out_min, out_max):
+def proportion(x: int, in_min: int, in_max: int, out_min: int, out_max: int) -> int:
     result = round((x - in_min) * (out_max - out_min) / (in_max - in_min)) + out_min
     if result > out_max:
         result = out_max
