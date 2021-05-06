f38dd4534da3db9cef577f1942c7858e7dc2b31f
Philip choi <devartmaker@gmail.com>
2021-05-06 22:08:05 +0900

    Update i2c_sacnner.py

--- i2c_scanner/i2c_sacnner.py
+++ i2c_scanner/i2c_sacnner.py
@@ -1,8 +1,6 @@
-import machine
+from machine import Pin, I2C
 
-sda = machine.Pin(16)
-scl = machine.Pin(17)
-i2c = machine.I2C(0, sda=sda, scl=scl, freq=400000)
+i2c = I2C(0, sda=Pin(16), scl=Pin(17))
  
 print('Scan i2c bus...')
 devices = i2c.scan()
@@ -10,7 +8,7 @@ devices = i2c.scan()
 if len(devices) == 0:
   print("No i2c device !")
 else:
-  print('i2c devices found:',len(devices))
+  print('i2c devices found:', len(devices))
  
   for device in devices:  
     print("Decimal address: ", device, " | Hexa address: ", hex(device))
